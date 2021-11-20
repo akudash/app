19dace86de2e5a20ad6e03d3596993c63e74e2a8
Andriy Kudashev <kudash@yahoo.com>
2021-11-20 11:00:56 -0600

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { error: null, loading: true, d1: "" };
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData = () => {
+    fetch("https://catfact.ninja/fact")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then((data) => this.setState({ d1: data.fact }))
+      .catch((e1) => {
+        this.setState({ ...this.state, error: e1 });
+      })
+      .finally(() => {
+        this.setState({ ...this.state, loading: false });
+      });
+  };
+
+  handleClick = () => {
+    this.fetchData();
+  };
+
+  render() {
+    if (this.state.loading) return <p>Loading...</p>;
+    if (this.state.error) return <p>Error!</p>;
+    return (
+      <div>
+        <p className="title1">A fact about cats:</p>
+        <p>{this.state.d1}</p>
+        <button onClick={this.handleClick}>Next fact</button>
+      </div>
+    );
+  }
 }
 
 export default App;
